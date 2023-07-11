b81eaebda17d80c1ab2342a4656e397702c6be9e
Renan Augusto Fengler <renan_fengler_93@hotmail.com>
2023-07-10 22:10:52 -0300

    Separar algoritmo de Fibonacci em arquivo separado
    Signed-off-by: Renan Augusto Fengler <renan_fengler_93@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,16 @@
-use std::{io, cmp::Ordering};
+use std::io;
+mod fibonacci;
 
 fn main() {
     println!("Number algorithms in Rust");
+
+    // Create a way to choose the program to run, and then come back to the menu
     println!("Insert how many fibonacci number to calculate");
 
     let mut count = String::new();
     io::stdin().read_line(&mut count).expect("Insert a valid number");
     let count: usize = count.trim().parse().expect("Could not parse input test");
 
-    fibonacci(&count);
+    fibonacci::fibonacci(&count);
 }
 
-pub fn fibonacci(count: &usize) {
-    let mut i = 1;
-    let mut vector: Vec<usize> = Vec::new();
-
-    vector.push(0);
-    vector.push(1);
-
-    loop {
-        match i.cmp(&count) {
-            Ordering::Equal => break,
-            Ordering::Greater => break,
-            Ordering::Less => {
-                let last = vector[i];
-                let second_to_last = vector[i - 1];
-                let current = last + second_to_last;
-
-                vector.push(current);
-                i += 1;
-            },
-        }
-    }
-
-    for x in vector.iter() {
-        println!("{}", x);
-    }
-}
